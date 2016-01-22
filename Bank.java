ee03b71378bedf7a3679792d2f249a0e3b0d5622
Matteo Longo <mat613@MacBook-Pro-di-Matteo-2.local>
2016-01-22 17:01:38 +0100

    Initial commit

---
+++ Bank.java
@@ -0,0 +1,14 @@
+package com.mybank.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by mat613 on 22/01/16.
+ */
+public class Bank {
+    List <Customer> customers=new ArrayList<>();
+
+    private Bank() {
+    }
+}
